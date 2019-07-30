d6fc5224f573358c646a7383bfee301a7d9e7bc3
Daniel Orio <danielislas3@live.com>
2019-07-29 19:40:16 -0500

    arreglado problema del login

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -3,10 +3,10 @@ import {Container,Header} from 'semantic-ui-react'
 
 import Navbar from './Navbar';
 
-const Profile = () => {
+const Profile = (props) => {
   return (
     <>
-      <Navbar/>
+      <Navbar {...props} />
       <Container text style={{ marginTop: '7em' }}>
       <Header as='h1'>Esto es el perfil</Header>
       <p>werwerwerwer</p>
